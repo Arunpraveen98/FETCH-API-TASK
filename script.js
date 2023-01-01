d4b1c65fc2aa10943b8ed646e967176bf5f00937
Arunpraveen98 <arunfbfb78@gmail.com>
2023-01-01 22:26:52 +0530

    weather displayed

--- script.js
+++ script.js
@@ -18,11 +18,14 @@ async function fetchApi() {
   //return promise object in readable stream
   // to handle this await is used.
   try {
+
     let res = await fetch("https://restcountries.com/v3.1/all");
     //after converting in Json Format it returns again Promise object
     //to handle this await is used.
     let JsonFormat = await res.json();
+    //----------------------------------------------------------------
 
+    //for loop to print data:-
     for (var i = 0; i < JsonFormat.length; i++) {
       var CountryName = JsonFormat[i].name.common;
       var countrycapital = JsonFormat[i].capital[x];
@@ -37,109 +40,79 @@ async function fetchApi() {
         throw new Error(`invalid:unable to read the data`);
       }
       //-------------------------------------------------------------
-      //*invoking a function:-
-      //restcountry function call:-
-      display(
-        `${CountryName}`,
-        `${countryflagimage}`,
-        `${countrycapital}`,
-        `${countryregion}`,
-        `${countrycode}`
-      );
-      //openWeather map function call:-
-      fetchWeather(latitude, longitude);
+      //appending innerHTML:-
+
+      div_row.innerHTML += `
+
+      <div class="col-lg-4">
+      <div class="card-group">
+      <div class="card" class="card border-dark mb-3" style="width: 18rem;">
+      <div class="card-header card-title">${CountryName}</div>
+      <img src="${countryflagimage}" id="flag-img" class="card-img-top" alt="CountryName:,${CountryName}" />
+      <div class="card-body">
+        <h5 class="card-title content">CAPITAL : ${countrycapital}</h5>
+        <h5 class="card-title content">REGION : ${countryregion}</h5>
+        <h5 class="card-title content">CODE : ${countrycode}</h5>
+        <button type="button" id="temp-btn" class="btn btn-primary" onclick=openWeather(${latitude},${longitude})>Click for weather</button>
+      </div>
+      </div>
+      </div>
+      </div>
+      </div>
+      </div>
+      `;
     }
-    //-------------------------------------------
+  } 
+  //catch-block:-
+  catch (error) {
+    console.log(error.message);
+  }
+} 
+//----------------------------------------------------------------
+//openweather api function:-
+
+async function openWeather(latitude, longitude) {
+  try {
+    var res = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=b9d07e8d66a7c9c78d47685619746207`
+    );
+    var result = await res.json();
+    var ans = result.main.temp;
+    var ans2 = document.getElementById("footer");
+    ans2.innerHTML = "";
+    ans2.append(`COUNTRY-WEATHER ⏩${ans}⏪`);
   } catch (error) {
     console.log(error.message);
   }
+
+  //----------------------------------------------------------------
 }
 
-//------------------------------------------------
-//async function call:-
 fetchApi();
-//------------------------------------------------
-
-//Openweather map data:-
-//^^^^^^^^^^^^^^^^^^^^
-
-async function fetchWeather(lat, lon) {
-  try {
-    var res = await fetch(
-      `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=b9d07e8d66a7c9c78d47685619746207`
-    );
-    var result = await res.json();
-    var Temp = result.main.temp;
-    console.log(Temp);
-  } catch (err) {
-    console.log(err);
-  }
-}
-
-//----------------------------------------------------------------
-
-//Appending rest countries data:-
-//^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-
-function display(
-  countryname,
-  countryflagimage,
-  countrycapital,
-  countryregion,
-  countrycode
-) {
-  div_row.innerHTML += `
-
-      <div class="col-lg-4">
-        <div class="card-group">
-        <div class="card" class="card border-dark mb-3" style="width: 18rem;">
-            <div class="card-header card-title">${countryname}</div>
-            <img src="${countryflagimage}" id="flag-img" class="card-img-top" alt="CountryName:,${countryname}" />
-            <div class="card-body">
-              <h5 class="card-title content">CAPITAL : ${countrycapital}</h5>
-              <h5 class="card-title content">REGION : ${countryregion}</h5>
-              <h5 class="card-title content">CODE : ${countrycode}</h5>
-              <button type="button" id="temp-btn" class="btn btn-primary">Click for weather</button>
-              <div class="card-footer bg-transparent" id="footer">TEMPERATURE :</div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-    </div>
-
-    `;
-  // function displayTemp()
-  // {
-  //   var data=
-  // }
-}
 
 //-------------------------------------------------------------------
+//appending elements:-
 
+//main-div
 var div_container = document.createElement("div");
 div_container.setAttribute("class", "container");
 
+//Heading-div
 var div_head = document.createElement("div");
 div_head.setAttribute("class", "heading");
 div_head.innerHTML = "COUNTRIES - DATA";
 
+//card-row
 var div_row = document.createElement("div");
 div_row.setAttribute("class", "row");
 
+//body-append
 document.body.append(div_container);
-div_container.append(div_head, div_row);
+div_container.append(div_row);
 
 //------------------------------------------------------------------
 
-
-
-
-
-
 //----------------------------------------------------------------
-
-
 
 //*How to Fetch data from api by using Promise:-
 // var createFunction = function () {
